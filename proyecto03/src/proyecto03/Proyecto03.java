d3ddc739dcfc70f0367e5d73c4d2f43f15666946
UTPL <servio.valverde.uemv@gmail.com>
2025-10-16 12:49:09 -0500

    Final commit

--- proyecto03/src/proyecto03/Proyecto03.java
+++ proyecto03/src/proyecto03/Proyecto03.java
@@ -14,7 +14,7 @@ public class Proyecto03 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("El IDE es GIT BASH y se usara Java como lenguaje de programacion");
     }
     
 }
